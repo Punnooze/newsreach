34c43042a654282fd1a137c95c9db327e011bfe8
Punnoose Punnen <punnoose88@gmail.com>
2024-10-26 00:32:20 +0530

    deployment fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,37 @@
 'use client';
-import homegif from '../assets/home.json';
-import lottie, { AnimationItem } from 'lottie-web';
+// import homegif from "../assets/home.json";
+// import lottie, { AnimationItem } from "lottie-web";
 import { useRouter } from 'next/navigation';
-import { useEffect, useRef } from 'react';
+// import { useEffect, useRef } from "react";
 
 export default function Home() {
   const router = useRouter();
-  const animationContainer = useRef<HTMLDivElement>(null);
+  // const animationContainer = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    let anim: AnimationItem | undefined;
+  // useEffect(() => {
+  //   if (typeof window === "undefined") return;
+  //   let anim: AnimationItem | undefined;
 
-    if (animationContainer.current && homegif) {
-      anim = lottie.loadAnimation({
-        container: animationContainer.current,
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        animationData: homegif,
-      });
-    } else {
-      console.error('Animation data is not defined or container is null');
-    }
+  //   if (animationContainer.current && homegif) {
+  //     anim = lottie.loadAnimation({
+  //       container: animationContainer.current,
+  //       renderer: "svg",
+  //       loop: true,
+  //       autoplay: true,
+  //       animationData: homegif,
+  //     });
+  //   } else {
+  //     console.error("Animation data is not defined or container is null");
+  //   }
 
-    return () => {
-      if (anim) anim.destroy();
-    };
-  }, []);
+  //   return () => {
+  //     if (anim) anim.destroy();
+  //   };
+  // }, []);
 
   return (
     <div className="mt-[100px] flex flex-col items-center justify-center">
-      <div className="h-[500px] relative" ref={animationContainer}></div>
+      {/* <div className="h-[500px] relative" ref={animationContainer}></div> */}
       <div className="absolute bottom-[100px] flex flex-col justify-center items-center">
         <h2>View your data!</h2>
         <button
